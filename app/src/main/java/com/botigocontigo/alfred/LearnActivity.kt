6d784cdc32e0f225ef85ca68e9213162e7b98bcb
pablo <psfutn@gmail.com>
2018-09-24 20:46:42 -0300

    learn (empty) activity

---
+++ app/src/main/java/com/botigocontigo/alfred/LearnActivity.kt
@@ -0,0 +1,22 @@
+package com.botigocontigo.alfred
+
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
+
+import kotlinx.android.synthetic.main.activity_learn.*
+
+class LearnActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_learn)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+        }
+    }
+
+}
