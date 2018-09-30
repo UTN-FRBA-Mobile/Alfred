fb2cee2e9d0d521f61261fb0295ff0602081996c
pablo <psfutn@gmail.com>
2018-09-30 17:43:31 -0300

    learn activity almost done...

--- app/src/main/java/com/botigocontigo/alfred/LearnActivity.kt
+++ app/src/main/java/com/botigocontigo/alfred/LearnActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
+import com.botigocontigo.alfred.learn.Article
 import com.botigocontigo.alfred.learn.ArticleAdapter
 
 import kotlinx.android.synthetic.main.activity_learn.*
@@ -18,12 +19,36 @@ class LearnActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
 
         articleList.layoutManager = LinearLayoutManager(this)
-        articleList.adapter = ArticleAdapter(this)
+        adapter = ArticleAdapter(this)
+        articleList.adapter = adapter
 
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
         }
+
+        loadArticles()
+    }
+
+    fun loadArticles() {
+        var title: String
+        var body: String
+        var article: Article
+        // article 1
+        title = "titulo 1"
+        body = "lorem ipsum 1"
+        article = Article(title, body)
+        adapter.addArticle(article)
+        // article 2
+        title = "titulo 2"
+        body = "lorem ipsum 2"
+        article = Article(title, body)
+        adapter.addArticle(article)
+        // article 3
+        title = "titulo 3"
+        body = "lorem ipsum 3"
+        article = Article(title, body)
+        adapter.addArticle(article)
     }
 
 }
