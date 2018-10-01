0f7be0889d02fcba04d20277f57596cd505765bb
facu <fluzko@gmail.com>
2018-10-01 20:00:46 -0300

    Login + Registrarse

--- app/src/main/java/com/botigocontigo/alfred/Login.kt
+++ app/src/main/java/com/botigocontigo/alfred/Login.kt
@@ -1,12 +1,22 @@
 package com.botigocontigo.alfred
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.*
 
 class Login : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        btn_login_registrarse.setOnClickListener {
+            startActivity(Intent(this, SignIn::class.java))
+        }
+
+        btn_login_ingresar.setOnClickListener {
+            startActivity(Intent(this, SplashActivity::class.java))
+        }
     }
 }
