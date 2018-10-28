2cf6cf191133d39fb49edc999422e82eb7d3573a
Julio Peralta <peraltajulioe@gmail.com>
2018-10-27 23:13:41 -0300

    Layout areas en fragment

--- app/src/main/java/com/botigocontigo/alfred/AppFragment.kt
+++ app/src/main/java/com/botigocontigo/alfred/AppFragment.kt
@@ -3,7 +3,7 @@ package com.botigocontigo.alfred
 import android.support.v4.app.Fragment
 import com.botigocontigo.alfred.tasks.TasksFragment
 
-class AppFragments : Fragment() {
+class AppFragments : Fragment(){
     object FRAGMENTS {
         operator fun get(position: Int): Fragment {
             return when(position) {
@@ -12,7 +12,7 @@ class AppFragments : Fragment() {
 //                1 -> TODO_FRAGMENT_SUGGESTIONS
                 2 -> TasksFragment()
 //                3 -> TODO_FRAGMENT_LEARN
-//                4 -> TODO_FRAGMENT_AREAS
+                4 -> AreasFragment()
 //                5 -> TODO_FRAGMENT_FODA
 //                6 -> TODO_FRAGMENT_RISKS
                 else -> TasksFragment()
