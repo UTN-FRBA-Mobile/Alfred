8d8ebdd0614c0e661979dad46d7c7bc6e864b8f5
Leonardo Manzella <leonardo.manzella@gmail.com>
2018-11-25 18:54:56 -0300

    Save all Risks

--- app/src/main/java/com/botigocontigo/alfred/backend/BotigocontigoApi.kt
+++ app/src/main/java/com/botigocontigo/alfred/backend/BotigocontigoApi.kt
@@ -99,7 +99,6 @@ class BotigocontigoApi(adapter: NetworkingAdapter, private val permissions: Perm
         return request
     }
 
-
     fun areasGetAll(): ApiRequest {
         val request = ApiRequest(this, "post", "methods/api.getAreas")
         applyPermissions(request)
@@ -123,6 +122,18 @@ class BotigocontigoApi(adapter: NetworkingAdapter, private val permissions: Perm
         applyPermissions(request)
         return request
     }
+
+    fun risksSaveAll(risks: Array<Risk>): ApiRequest {
+        val request = ApiRequest(this, "post", "methods/api.saveRisks")
+        applyPermissions(request)
+
+        //FIXME if this fails we need to transform manually pDeOcurrecia and cDeDeteccion fields to change their name
+        val gson = Gson()
+        val areasJSONObject= gson.toJson(risks)
+        request.put("risks", areasJSONObject)
+        return request
+    }
+
     fun fodaGetAll():ApiRequest {
         val request = ApiRequest(this,"post","methods/api.getSwot")
         applyPermissions(request)
