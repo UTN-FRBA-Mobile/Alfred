8ddc97bfcb8618b3c6192621e1f1b1e793df8ebc
Leonardo Manzella <leonardo.manzella@gmail.com>
2018-12-08 10:46:51 -0300

    WIP Improve Sign In

--- app/src/main/java/com/botigocontigo/alfred/SignIn.kt
+++ app/src/main/java/com/botigocontigo/alfred/SignIn.kt
@@ -4,9 +4,12 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
+import com.botigocontigo.alfred.backend.UserShareDeserealizer
+import com.google.gson.GsonBuilder
 import kotlinx.android.synthetic.main.activity_login.*
 import kotlinx.android.synthetic.main.activity_sign_in.*
 import org.json.JSONObject
@@ -22,14 +25,16 @@ class SignIn : AppCompatActivity() {
         }
 
         btn_sing_in_registrarse.setOnClickListener {
-            // startActivity(Intent(this, Login::class.java))
+            if(user_password_signIn.text.toString() != user_password_signIn_confirm.text.toString()){
+                Toast.makeText(this, "No coinciden las contraseñas", Toast.LENGTH_LONG).show()
+                return@setOnClickListener
+            }
+
 
             /**
              * Llamada POST que envia un JSONObject y devuelve un JSONobject
-
-
             Log.i(Login.LOG_TAG, user_nombre.text.toString())
-            Log.i(Login.LOG_TAG, user_apellido.text.toString())
+            Log.i(Login.LOG_TAG, user_password_signIn.text.toString())
             Log.i(Login.LOG_TAG, user_email.text.toString())
             Log.i(Login.LOG_TAG, "jsonObjectRequestPost")
              */
@@ -43,7 +48,7 @@ class SignIn : AppCompatActivity() {
 
             val jsonObject = JSONObject()
             jsonObject.put("name", user_nombre.text.toString())
-            jsonObject.put("password",  user_apellido.text.toString())
+            jsonObject.put("password",  user_password_signIn.text.toString())
             jsonObject.put("email",  user_email.text.toString())
 
             Log.i(Login.LOG_TAG, "JsonObject es: $jsonObject")
@@ -52,6 +57,7 @@ class SignIn : AppCompatActivity() {
                     Response.Listener { response ->
                         Log.i(Login.LOG_TAG, "Response es: $response")
                         startActivity(Intent(this, Login::class.java))
+                        logInSimplified(user_email.text.toString(), user_password_signIn.text.toString() );
                     },
                     Response.ErrorListener { error ->
                         error.printStackTrace()
@@ -62,5 +68,68 @@ class SignIn : AppCompatActivity() {
             // Add the request to the RequestQueue.
             queue.add(jsonObjectRequest)
         }
+    }
+
+    private fun logInSimplified(email: String, password: String) {
+        val mypreference = MyPreferences(this)
+        val queue = Volley.newRequestQueue(this)
+        val url = "http://178.128.229.73:3300/methods/api.login/"
+
+        Log.i(Login.LOG_TAG, email)
+        Log.i(Login.LOG_TAG, password)
+
+        val jsonObject = JSONObject()
+        jsonObject.put("email", email)
+        jsonObject.put("password", password)
+
+        Log.i(Login.LOG_TAG, "JsonObject es: $jsonObject")
+        val jsonObjectRequest = JsonObjectRequest(url, jsonObject,
+                Response.Listener { response ->
+                    try {
+
+                        val gsonBuilder = GsonBuilder().serializeNulls()
+                        gsonBuilder.registerTypeAdapter(UserShare::class.java, UserShareDeserealizer())
+                        val gson = gsonBuilder.create()
+
+                        val userShareJsonParsed: UserShare = gson.fromJson(response.toString(), UserShare::class.java)
+                        if (!userShareJsonParsed.sucess!!) throw Exception("Not Logued In");
+
+                        Log.i(Login.LOG_TAG, "userShareJsonParsed.email es:" + userShareJsonParsed.email)
+                        // Log.i(LOG_TAG, "userShareJsonParsed.userId es:" +  userShareJsonParsed.userId)
+
+                        //Se guardan valores del Email en Share Preferen
+                        mypreference.setUserEmail(userShareJsonParsed.email!!)
+                        val userEmail = mypreference.getUserEmail()
+                        Log.i(Login.LOG_TAG, "El email es: $userEmail")
+
+                        Toast.makeText(this, "Bienvenido!", Toast.LENGTH_LONG).show()
+
+
+                        //Se guardan valores del Email en Share Preferen
+                        mypreference.setUserId(userShareJsonParsed.userId!!)
+                        val userID = mypreference.getUserId()
+                        Log.i(Login.LOG_TAG, "El userID es: $userID")
+
+
+                        startActivity(Intent(this, MenuActivity::class.java))
+                    } catch (e: Exception) {
+                        e.printStackTrace()
+                        Log.e(Login.LOG_TAG, "Error al Login.")
+                        var errorString = "Hubo un error de conexión, por favor Ingrese de forma manual"
+                        Toast.makeText(this, errorString, Toast.LENGTH_LONG).show()
+                        //TODO fixme hacer que valla a activity login
+                    }
+                },
+                Response.ErrorListener { error ->
+                    error.printStackTrace()
+                    Log.e(Login.LOG_TAG, "Error al Login.")
+                    var errorString = "Hubo un error de conexión, por favor Ingrese de forma manual"
+                    Toast.makeText(this, errorString, Toast.LENGTH_LONG).show()
+                    //TODO fixme hacer que valla a activity login
+                }
+        )
+
+        // Add the request to the RequestQueue.
+        queue.add(jsonObjectRequest)
     }
 }
